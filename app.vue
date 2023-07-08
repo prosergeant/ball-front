f9add7b3b0e6cc7eb7f24547055071f390d040f7
prosergeant <sberendeyev@gmail.com>
2023-07-08 19:12:07 +0600

    block zoom

--- app.vue
+++ app.vue
@@ -14,3 +14,41 @@
     background: $background;
 }
 </style>
+
+<script setup>
+
+useHead({
+    meta: [
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        }
+    ]
+})
+
+onMounted(() => {
+    if(window) {
+        document.addEventListener('gesturestart', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 1;
+        });
+
+        document.addEventListener('gesturechange', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 1;
+        });
+
+        document.addEventListener('gestureend', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 1;
+        });
+
+        document.ondblclick = function(e) {
+            e.preventDefault();
+        }
+    }
+})
+</script>
