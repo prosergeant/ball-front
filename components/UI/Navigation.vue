60049d664cda5206700e75139dbfdd2f55936768
prosergeant <sberendeyev@gmail.com>
2023-07-13 23:55:39 +0600

    Navigation.vue

---
+++ components/UI/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="navigation">
+        <div class="left" @click="$router.push(goTo)">
+            <UIIcon icon="chevron-left" color="white" />
+            <p>{{ title }}</p>
+        </div>
+        <UIIcon icon="search" color="white" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    title: string
+    goTo: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.navigation {
+    height: 26px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 10px;
+
+    .left {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+
+        p {
+            font-size: 20px;
+            font-weight: 400;
+            line-height: 24px;
+            letter-spacing: 0;
+            text-align: left;
+            color: white;
+        }
+        i {
+            width: 14px;
+            height: 14px;
+        }
+    }
+
+
+
+    i {
+        width: 26px;
+        height: 26px;
+    }
+
+
+}
+</style>
