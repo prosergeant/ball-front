6c2affbba730947ff89f2770473d2930459acac9
prosergeant <sberendeyev@gmail.com>
2023-07-21 14:02:17 +0600

    fix user cookie

--- store/auth.ts
+++ store/auth.ts
@@ -2,17 +2,17 @@ import {defineStore} from 'pinia'
 
 export const authStore = defineStore('auth', () => {
     const auth_coockie = useCookie('is_auth')
+    const user_coockie = useCookie('user')
     const is_auth = ref(auth_coockie.value || false)
-    const user_info = ref(useCookie('user') || {})
+    const user_info = ref(user_coockie.value || {})
     const access_token = ref('')
 
     function logout() {
-        const _user = useCookie('user')
         const _refresh = useCookie('refresh')
         const _access = useCookie('access')
         auth_coockie.value = null
+        user_coockie.value = null
         is_auth.value = false
-        _user.value = null
         _refresh.value = null
         _access.value = null
         access_token.value = ''
