f6f68a7916de562f1464a81f32d6e9530ec35c21
prosergeant <sberendeyev@gmail.com>
2023-08-24 11:52:20 +0600

    add ssl v9

--- composables/baseUrl.ts
+++ composables/baseUrl.ts
@@ -1,4 +1,4 @@
 // export default 'http://192.168.1.113:8000'
-export const justURL = 'http://95.164.19.83'
-export default `${justURL}:8000`
+export const justURL = 'https://bronkz.app' //'http://95.164.19.83'
+export default `${justURL}/api`
 
