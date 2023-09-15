da0d253bce70a39784225d1a4fe570802cb5f3b4
prosergeant <sberendeyev@gmail.com>
2023-09-16 03:54:11 +0600

    test page

--- pages/field/[id].vue
+++ pages/field/[id].vue
@@ -6,6 +6,7 @@ div(style="color: white;")
     p error: {{ error }}
     a(href="https://google.com" target="_blank") lupa
     UIButton(@click="fcm") send fcm token
+    UIButton(@click="addCalendar") add calendar
 </template>
 
 <script setup lang="ts">
@@ -28,5 +29,32 @@ const fcm = () => {
     myFetch(`/change-fcm-token/?token=${token}`, {
         method: 'POST',
     })
+}
+
+const addCalendar = () => {
+    let ics = `BEGIN:VCALENDAR
+METHOD:PUBLISH
+VERSION:2.0
+X-WR-CALNAME:Calendar
+PRODID:-//bronkz.app//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:pupalupa
+DTSTAMP:20230913T042324
+DTSTART:20230914T070000Z
+DTEND:20230914T080000Z
+SUMMARY:Встреча
+DESCRIPTION:
+END:VEVENT
+END:VCALENDAR`
+
+    const title = "newEvent.ics";
+    const uri = "data:text/calendar;charset=utf8," + encodeURIComponent(ics)
+    const link = document.createElement('a')
+    link.href = uri
+    link.download = title
+    link.target = '_BLANK'
+    document.body.appendChild(link)
+    link.click();
+    link.remove();
 }
 </script>
